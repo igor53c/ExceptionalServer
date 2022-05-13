b36a1c6c3ff91040270e3311e7fde21ce498c60a
Igor <nesto@nesto.co>
2022-05-13 20:27:50 +0200

    Add project files.

---
+++ ExceptionalServer.cpp
@@ -0,0 +1,66 @@
+// ExceptionalServer.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+#include <iostream>
+#include <exception>
+#include <string>
+#include <stdexcept>
+#include <vector>
+#include <cmath>
+using namespace std;
+
+class Server {
+private:
+	static int load;
+public:
+	static int compute(long long A, long long B) {
+		load += 1;
+		if (A < 0) {
+			throw invalid_argument("A is negative");
+		}
+		vector<int> v(A, 0);
+		int real = -1, cmplx = sqrt(-1);
+		if (B == 0) throw 0;
+		real = (A / B) * real;
+		int ans = v.at(B);
+		return real + A - B * ans;
+	}
+	static int getLoad() {
+		return load;
+	}
+};
+int Server::load = 0;
+
+int main() {
+	int T; cin >> T;
+	while (T--) {
+		long long A, B;
+		cin >> A >> B;
+
+		try {
+			auto* ptr = new long long;
+
+			ptr = &A;
+
+			ptr = &B;
+
+			
+			cout << Server::compute(A, B) << endl;
+		}
+		catch (bad_alloc& e)
+		{
+			cout << "Not enough memory" << endl;
+		}
+		catch (exception& e)
+		{
+			cout << "Exception: " << e.what() << endl;
+		}
+		catch (...)
+		{
+			cout << "Other Exception" << endl;
+		}
+	}
+
+	cout << Server::getLoad() << endl;
+
+	return EXIT_SUCCESS;
+}
